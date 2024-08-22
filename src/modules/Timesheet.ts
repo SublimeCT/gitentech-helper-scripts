decedbe85ffff1dab14fa156622707ca71f2b3a6
kuidi‮‮ <hellosc@qq.com>
2024-08-22 17:18:26 +0800

    feat(Timesheet): 在日历翻页时增加 loading 判断; 修复初次初始化时数据未填充的问题

--- src/modules/Timesheet.ts
+++ src/modules/Timesheet.ts
@@ -9,9 +9,9 @@ export class TimesheetModule implements ApplicationModule {
   scope: any = null
   async onLoad() {
     if (!checkRoute('/Fill_ByCalendar.aspx', this.page)) return this.unMounted()
-    if (!this.initialized) this.init()
+    if (!this.initialized) this.initModule()
   }
-  async init() {
+  async initModule() {
     this.initialized = true
     const angular = await waitFor(() => (window as any).angular)
     if (!angular) throw new Error('Missing angular object')
@@ -19,13 +19,13 @@ export class TimesheetModule implements ApplicationModule {
     if (!el) throw new Error('Missing timesheet tab page element')
     this.scope = await waitFor(() => angular.element(el).scope())
     console.warn('timesheet', this.scope)
-    this.modifyWeekList()
+    delay(1000).then(() => this.modifyWeekList())
     this.interceptNextAndRevious()
   }
+  /** 更新日历组件中的 weekList 数据, 并触发 UI 层更新 */
   modifyWeekList() {
     for (const row of this.scope.MetaData.ResultCalender.WeekList) {
       const satItem = row[row.length - 1]
-      console.log(row)
       if (!Reflect.has(satItem, 'isWorkDay')) continue // 忽略非本月的日期
       if (satItem.dayState !== '0') continue // 忽略已填写的日期
       if (satItem.isholiday !== 'True') {
@@ -34,18 +34,27 @@ export class TimesheetModule implements ApplicationModule {
         console.log('符合条件的周六', satItem)
       }
     }
+    this.clickToUpdateElement()
+  }
+  clickToUpdateElement() {
+    const li = document.querySelector<HTMLDivElement>('.calenderDate')
+    li?.click()
+  }
+  /** 确保页面内已经没有了 loading 元素 */
+  ensureNoLoading() {
+    return waitFor(() => !document.querySelector('.loading-indicator-overlay'))
   }
   interceptNextAndRevious() {
     const _this = this
     this.scope._$next = this.scope.next
     this.scope.next = function(...args: any[]) {
       this._$next(...args)
-      delay(1500).then(() => _this.modifyWeekList())
+      delay(1500).then(_this.ensureNoLoading).then(() => _this.modifyWeekList())
     }
     this.scope._$revious = this.scope.revious
     this.scope.revious = function(...args: any[]) {
       this._$revious(...args)
-      delay(1500).then(() => _this.modifyWeekList())
+      delay(1500).then(_this.ensureNoLoading).then(() => _this.modifyWeekList())
     }
   }
   routeChange(): void {
